4cbaec43b3af2c36cf85ad139b24d2bcfd224a64
Gilang Permana <166970914+gpxairdrop@users.noreply.github.com>
2024-10-17 03:56:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ walletKit.on('session_authenticate', async payload => {
   // 4. Sign the authentication message(s) to create a verifiable authentication object(s)
   // 5. Approve the authentication request with the authentication object(s)
 })
+import { populateAuthPayload } from "@walletconnect/utils";
+
+// EVM chains that your wallet supports
+const supportedChains = ["eip155:11155111", "eip155:17000"];
+// EVM methods that your wallet supports
+const supportedMethods = ["personal_sign", "eth_sendTransaction", "eth_signTypedData"];
+// Populate the authentication payload with the supported chains and methods
+const authPayload = populateAuthPayload({
+  authPayload: payload.params.authPayload,
+  chains: supportedChains,
+  methods: supportedMethods,
+});
+// Prepare the user's address in CAIP10(https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-10.md) format
+const iss = `eip155:1:0x0Df6d2a56F90e8592B4FfEd587dB3D5F5ED9d6ef`;
+// Now you can use the authPayload to format the authentication message
+const message = walletKit.formatAuthMessage({
+  request: authPayload,
+  iss
+});
+
+// Present the authentication message to the user
+...
