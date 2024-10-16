424da629bf8e467ae6889ea1d2890553da8bd696
Gilang Permana <166970914+gpxairdrop@users.noreply.github.com>
2024-10-17 04:38:26 +0700

    Update web3.js

--- web3.js
+++ web3.js
@@ -3,12 +3,35 @@
 /* To connect using MetaMask */
 async function connect() {
  if (window.ethereum) {
- 
-  await window.ethereum.request({ method: "eth_requestAccounts" });
-  window.web3 = new Web3(window.ethereum);
-  
- } else {
-  console.log("No wallet");
- }
+import { createAppKit } from '@reown/appkit/react'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { arbitrum, mainnet } from '@reown/appkit/networks'
+
+// 1. Get projectId
+const projectId = 'fd4d93557f7fb9680d03266b1f45a117';
+
+// 2. Set the networks
+const networks = [arbitrum, mainnet];
+
+// 3. Create a metadata object - optional
+const metadata = {
+  name: 'web3 givpn',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
 }
-</script>
+
+// 4. Create a AppKit instance
+createAppKit({
+  adapters: [new ethersAdapter()],
+  networks,
+  metadata,
+  projectId,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+export default function App() {
+  return <YourApp /> // Configure the <w3m-button> or a similar button inside
+}
