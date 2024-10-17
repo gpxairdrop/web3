2a2a9500fb897ec2206046e4501a0b348f5dbb15
Gilang Permana <166970914+gpxairdrop@users.noreply.github.com>
2024-10-17 07:25:44 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { arbitrum, mainnet, optimism, polygon, zkSync, sepolia } from '@reown/appkit/networks'
+import { arbitrum, mainnet } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { createAppKit } from '@reown/appkit'
-import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 
 // @ts-expect-error 1. Get projectId
 const projectId = import.meta.env.VITE_PROJECT_ID
@@ -8,44 +8,31 @@ if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set')
 }
 
-// Create adapter
-const ethersAdapter = new EthersAdapter()
-
-// Instantiate AppKit
-const modal = createAppKit({
-  adapters: [ethersAdapter],
-  networks: [arbitrum, mainnet, optimism, polygon, zkSync, sepolia],
+// 2. Create Wagmi adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks: [mainnet, arbitrum],
   projectId
 })
 
-// Trigger modal programaticaly
+// 3. Create modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  metadata: {
+    name: 'Html Example',
+    description: 'Html Example',
+    url: 'https://reown.com/appkit',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  },
+  networks: [mainnet, arbitrum],
+  projectId,
+  themeMode: 'light'
+})
+
+// 4. Trigger modal programaticaly
 const openConnectModalBtn = document.getElementById('open-connect-modal')
 const openNetworkModalBtn = document.getElementById('open-network-modal')
 
 openConnectModalBtn.addEventListener('click', () => modal.open())
 openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
 
-const updateElement = (id, content) => {
-  const element = document.getElementById(id)
-  if (element) {
-    element.innerHTML = content
-  }
-}
-
-const intervalId = setInterval(() => {
-  updateElement('getError', JSON.stringify(modal.getError(), null, 2))
-  updateElement('getChainId', JSON.stringify(modal.getChainId(), null, 2))
-  updateElement('getAddress', JSON.stringify(modal.getAddress(), null, 2))
-  updateElement('switchNetwork', JSON.stringify(modal.switchNetwork(), null, 2))
-  updateElement('getIsConnected', JSON.stringify(modal.getIsConnected(), null, 2))
-  updateElement('getWalletProvider', JSON.stringify(modal.getWalletProvider(), null, 2))
-  updateElement('getWalletProviderType', JSON.stringify(modal.getWalletProviderType(), null, 2))
-}, 2000)
-
-window.addEventListener('beforeunload', () => {
-  clearInterval(intervalId)
-})
-
-modal.subscribeProvider(state => {
-  updateElement('subscribeProvider', JSON.stringify(state, null, 2))
-})
+// 5. Alternatively use w3m component buttons (see index.html)
